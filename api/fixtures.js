6b9cc0dfbffc53bf70318e38b3dcc4eafaaea972
phlair222-max <phlair222@gmail.com>
2026-05-16 04:01:07 +0100

    Update fixtures.js

--- api/fixtures.js
+++ api/fixtures.js
@@ -1,15 +1,17 @@
 export default async function handler(req, res) {
-  const { competitions, dateFrom, dateTo } = req.query;
+  var competitions = req.query.competitions;
+  var dateFrom = req.query.dateFrom;
+  var dateTo = req.query.dateTo;
 
-  const url = `https://api.football-data.org/v4/matches?competitions=${competitions}&dateFrom=${dateFrom}&dateTo=${dateTo}`;
+  var url = 'https://api.football-data.org/v4/matches?competitions=' + competitions + '&dateFrom=' + dateFrom + '&dateTo=' + dateTo;
 
-  const response = await fetch(url, {
+  var response = await fetch(url, {
     headers: {
-      'X-Auth-Token': process.env.REACT_APP_FOOTBALLDATA_KEY,
+      'X-Auth-Token': process.env.REACT_APP_FOOTBALLDATA_KEY || '36f65f997b7c4e698fe90b8741745505',
     },
   });
 
-  const data = await response.json();
+  var data = await response.json();
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(response.status).json(data);
